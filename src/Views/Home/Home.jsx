108d48b1cf0015bc95ccf2b9ad2f442b2b19b588
SayaliPawar1312 <sayalipawar1312@gmail.com>
2026-04-23 14:49:29 +0530

    add a seperatr container for images

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -3,7 +3,6 @@ import Footer from "./../../components/Footer/Footer";
 import "./Home.css";
 import BodyContainer from "../../components/BodyContainer/BodyContainer";
 
-
 import Cake1 from "../../assets/cake1.webp";
 import Cake2 from "../../assets/cake2.webp";
 import Cake3 from "../../assets/cake3.webp";
@@ -12,51 +11,58 @@ import Cake5 from "../../assets/cake5.webp";
 import Cake6 from "../../assets/cake6.webp";
 import Cake7 from "../../assets/cake7.webp";
 import Cake8 from "../../assets/cake8.webp";
+import Cake9 from "../../assets/cake9.webp";
 
 const Products = [
   {
     id: 1,
-    title: "Chocolate Cake",
+    title: "Brownies",
     image: Cake1
   },
   {
     id: 2,
-    title: "Vanilla Cake",
+    title: "Croissant, Danishes & Muffins",
     image: Cake2
   },
   {
     id: 3,
-    title: "Strawberry Cake",
+    title: "Desserts & Cupcakes",
     image: Cake3
   },
   {
     id: 4,
-    title: "Red Velvet Cake",
+    title: "Butterscotch Cake",
     image: Cake4
   },
   {
     id: 5,
-    title: "Carrot Cake",
+    title: "Cup Cake",
     image: Cake5
   },
-  
-   {
+
+  {
     id: 6,
-    title: "Carrot Cake",
+    title: "Red-Velvet",
     image: Cake6
   },
 
-   {
+  {
     id: 7,
-    title: "Carrot Cake",
+    title: "Beverage",
     image: Cake7
   },
 
-   {
+  {
     id: 8,
-    title: "Carrot Cake",
+    title: "Mango Specials",
     image: Cake8
-  }
+  },
+
+  {
+    id: 9,
+    title: "Gifting",
+    image: Cake9
+  },
 ];
 
 function Home() {
@@ -69,15 +75,18 @@ function Home() {
           <h1 className="home-title">Welcome to Bakery Shop</h1>
           <p className="home-subtitle"><i>Fresh bakery treats made daily just for you.</i></p>
         </div>
+      </BodyContainer>
 
+      <BodyContainer >
         <div className="product-container">
+
           {Products.map((product) => (
             <div key={product.id} className="product-card">
               <div className="card-image">
                 <img src={product.image} alt={product.title} />
               </div>
               <div className="card-content">
-              <center> <h3>{product.title}</h3></center>
+                <center> <h3>{product.title}</h3></center>
               </div>
             </div>
           ))}
