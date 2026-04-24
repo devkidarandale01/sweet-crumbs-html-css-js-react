4fa7e924869d020c6a51b6d544d01b528b77dd5f
rutujaraut7499637769 <rutujar988@gmail.com>
2026-04-24 22:16:31 +0530

    remove rating option

--- src/Views/CakeGallery/CakeGallery.jsx
+++ src/Views/CakeGallery/CakeGallery.jsx
@@ -18,18 +18,18 @@ import cake11 from "../../assets/gallery11.webp";
 import cake12 from "../../assets/gallery12.webp";
 
 const cakesData = [
-  { id: 1, name: "Fruit Cream Cake", price: "₹500", desc: "Fresh fruit topped soft cake", rating: 4, bestseller: true, img: cake1 },
-  { id: 2, name: "Vanilla cream cake", price: "₹600", desc: "Vanilla cream cake", rating: 5, img: cake2 },
-  { id: 3, name: "Chocolate Jar", price: "₹550", desc: "Layered chocolate dessert jar", rating: 4, bestseller: true, img: cake3 },
-  { id: 4, name: "Chocolate Pastry + Coffee", price: "₹700", desc: "  Rich chocolate pastry served with hot coffee .", rating: 3, img: cake4 },
-  { id: 5, name: "Cupcake", price: "₹400", desc: "Soft cupcake with cream topping", rating: 4, img: cake5 },
-  { id: 6, name: "Chocolate Pastry", price: "₹100", desc: "Rich chocolate layered pastry", rating: 5, bestseller: true, img: cake6 },
-  { id: 7, name: "Almond Cake", price: "₹450", desc: "Cake with crunchy almond flakes", rating: 4, img: cake7 },
-  { id: 8, name: "Loaf Bread", price: "₹550", desc: "Fresh soft baked bread loaf", rating: 3, img: cake8 },
-  { id: 9, name: "Chocolate Cookies", price: "₹500", desc: "Crunchy chocolate cookies", rating: 4, img: cake9 },
-  { id: 10, name: "Palmier", price: "₹800", desc: "Crispy puff pastry", rating: 5, img: cake10 },
-  { id: 11, name: "Jam Cookies", price: "₹750", desc: "Cookies filled with sweet jam", rating: 3, img: cake11 },
-  { id: 12, name: "Donut", price: "₹650", desc: "Soft donut with sprinkles", rating: 4, img: cake12 }
+  { id: 1, name: "Fruit Cream Cake", price: "₹500", desc: "Fresh fruit topped soft cake",  bestseller: true, img: cake1 },
+  { id: 2, name: "Vanilla cream cake", price: "₹600", desc: "Vanilla cream cake",  img: cake2 },
+  { id: 3, name: "Chocolate Jar", price: "₹550", desc: "Layered chocolate dessert jar",  bestseller: true, img: cake3 },
+  { id: 4, name: "Chocolate Pastry + Coffee", price: "₹700", desc: "  Rich chocolate pastry served with hot coffee .",  img: cake4 },
+  { id: 5, name: "Cupcake", price: "₹400", desc: "Soft cupcake with cream topping",  img: cake5 },
+  { id: 6, name: "Chocolate Pastry", price: "₹100", desc: "Rich chocolate layered pastry", bestseller: true, img: cake6 },
+  { id: 7, name: "Almond Cake", price: "₹450", desc: "Cake with crunchy almond flakes", img: cake7 },
+  { id: 8, name: "Loaf Bread", price: "₹550", desc: "Fresh soft baked bread loaf",  img: cake8 },
+  { id: 9, name: "Chocolate Cookies", price: "₹500", desc: "Crunchy chocolate cookies",  img: cake9 },
+  { id: 10, name: "Palmier", price: "₹800", desc: "Crispy puff pastry",  img: cake10 },
+  { id: 11, name: "Jam Cookies", price: "₹750", desc: "Cookies filled with sweet jam", img: cake11 },
+  { id: 12, name: "Donut", price: "₹650", desc: "Soft donut with sprinkles",  img: cake12 }
 ];
 
 function CakeGallery() {
